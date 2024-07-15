080dff50e5e6174d09000a1d1d061e1c501e17cf
Ayliase-official <praghunath.official@gmail.com>
2024-07-15 16:00:42 +0530

    added remaps for easier pane manipulation

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -1,3 +1,38 @@
+-- leader key
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
 
+-- netrw
+vim.keymap.set("n", "<leader>e", ":Ex<CR>")
+
+-- multi pane operations
+vim.keymap.set("n", "<leader>wh", ":split<CR>")
+vim.keymap.set("n", "<leader>wv", ":vs<CR>")
+vim.keymap.set("n", "<leader>ww", ":wincmd w<CR>")
+vim.keymap.set("n", "<leader>h", ":wincmd h<CR>")
+vim.keymap.set("n", "<leader>j", ":wincmd j<CR>")
+vim.keymap.set("n", "<leader>k", ":wincmd k<CR>")
+vim.keymap.set("n", "<leader>l", ":wincmd l<CR>")
+vim.keymap.set("n", "<leader>q", ":close<CR>")
+vim.keymap.set("n", "<leader>Q", ":only<CR>")
+
+-- clipboard yanking
+-- vim.api.nvim_set_keymap("n", "y", '"+y', { noremap = true, silent = true });
+-- vim.api.nvim_set_keymap("n", "yy", '"+y', { noremap = true, silent = true });
+-- vim.api.nvim_set_keymap("v", "y", '"+y', { noremap = true, silent = true });
+
+-- Yank to clipboard by default
+vim.api.nvim_set_keymap('n', 'y', '"+y', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', 'y', '"+y', { noremap = true, silent = true })
+
+-- Optional: Also map yy to copy the entire line to clipboard
+vim.api.nvim_set_keymap('n', 'yy', '"+yy', { noremap = true, silent = true })
+
+local function show_file_type()
+    local file_type = vim.bo.filetype
+    print("File type: " .. file_type)
+end
+
+-- quick commenting only works with C
+vim.keymap.set("x", "<leader>c", "I// <Esc>")
+vim.keymap.set("n", "<leader>c", "I// <Esc>")
+vim.keymap.set("v", "<leader>c", "I// <Esc>")
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -49,8 +49,8 @@ local function save_profiles(threshold)
 end
 
 time([[Luarocks path setup]], true)
-local package_path_str = "/root/.cache/nvim/packer_hererocks/2.1.1716656478/share/lua/5.1/?.lua;/root/.cache/nvim/packer_hererocks/2.1.1716656478/share/lua/5.1/?/init.lua;/root/.cache/nvim/packer_hererocks/2.1.1716656478/lib/luarocks/rocks-5.1/?.lua;/root/.cache/nvim/packer_hererocks/2.1.1716656478/lib/luarocks/rocks-5.1/?/init.lua"
-local install_cpath_pattern = "/root/.cache/nvim/packer_hererocks/2.1.1716656478/lib/lua/5.1/?.so"
+local package_path_str = "/home/insait/.cache/nvim/packer_hererocks/2.1.1713484068/share/lua/5.1/?.lua;/home/insait/.cache/nvim/packer_hererocks/2.1.1713484068/share/lua/5.1/?/init.lua;/home/insait/.cache/nvim/packer_hererocks/2.1.1713484068/lib/luarocks/rocks-5.1/?.lua;/home/insait/.cache/nvim/packer_hererocks/2.1.1713484068/lib/luarocks/rocks-5.1/?/init.lua"
+local install_cpath_pattern = "/home/insait/.cache/nvim/packer_hererocks/2.1.1713484068/lib/lua/5.1/?.so"
 if not string.find(package.path, package_path_str, 1, true) then
   package.path = package.path .. ';' .. package_path_str
 end
@@ -76,72 +76,72 @@ time([[Defining packer_plugins]], true)
 _G.packer_plugins = {
   LuaSnip = {
     loaded = true,
-    path = "/root/.local/share/nvim/site/pack/packer/start/LuaSnip",
+    path = "/home/insait/.local/share/nvim/site/pack/packer/start/LuaSnip",
     url = "https://github.com/L3MON4D3/LuaSnip"
   },
   ["autoclose.nvim"] = {
     loaded = true,
-    path = "/root/.local/share/nvim/site/pack/packer/start/autoclose.nvim",
+    path = "/home/insait/.local/share/nvim/site/pack/packer/start/autoclose.nvim",
     url = "https://github.com/m4xshen/autoclose.nvim"
   },
   ["cmp-nvim-lsp"] = {
     loaded = true,
-    path = "/root/.local/share/nvim/site/pack/packer/start/cmp-nvim-lsp",
+    path = "/home/insait/.local/share/nvim/site/pack/packer/start/cmp-nvim-lsp",
     url = "https://github.com/hrsh7th/cmp-nvim-lsp"
   },
   flexoki = {
     loaded = true,
-    path = "/root/.local/share/nvim/site/pack/packer/start/flexoki",
+    path = "/home/insait/.local/share/nvim/site/pack/packer/start/flexoki",
     url = "https://github.com/kepano/flexoki-neovim"
   },
   ["lsp-zero.nvim"] = {
     loaded = true,
-    path = "/root/.local/share/nvim/site/pack/packer/start/lsp-zero.nvim",
+    path = "/home/insait/.local/share/nvim/site/pack/packer/start/lsp-zero.nvim",
     url = "https://github.com/VonHeikemen/lsp-zero.nvim"
   },
   ["mason.nvim"] = {
     loaded = true,
-    path = "/root/.local/share/nvim/site/pack/packer/start/mason.nvim",
+    path = "/home/insait/.local/share/nvim/site/pack/packer/start/mason.nvim",
     url = "https://github.com/williamboman/mason.nvim"
   },
   ["noice.nvim"] = {
     loaded = true,
-    path = "/root/.local/share/nvim/site/pack/packer/start/noice.nvim",
+    path = "/home/insait/.local/share/nvim/site/pack/packer/start/noice.nvim",
     url = "https://github.com/folke/noice.nvim"
   },
   ["nui.nvim"] = {
     loaded = true,
-    path = "/root/.local/share/nvim/site/pack/packer/start/nui.nvim",
+    path = "/home/insait/.local/share/nvim/site/pack/packer/start/nui.nvim",
     url = "https://github.com/MunifTanjim/nui.nvim"
   },
   ["nvim-cmp"] = {
     loaded = true,
-    path = "/root/.local/share/nvim/site/pack/packer/start/nvim-cmp",
+    path = "/home/insait/.local/share/nvim/site/pack/packer/start/nvim-cmp",
     url = "https://github.com/hrsh7th/nvim-cmp"
   },
   ["nvim-lspconfig"] = {
     loaded = true,
-    path = "/root/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
+    path = "/home/insait/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
     url = "https://github.com/neovim/nvim-lspconfig"
   },
   ["nvim-treesitter"] = {
     loaded = true,
-    path = "/root/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
+    path = "/home/insait/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
     url = "https://github.com/nvim-treesitter/nvim-treesitter"
   },
   ["packer.nvim"] = {
     loaded = true,
-    path = "/root/.local/share/nvim/site/pack/packer/start/packer.nvim",
+    path = "/home/insait/.local/share/nvim/site/pack/packer/start/packer.nvim",
     url = "https://github.com/wbthomason/packer.nvim"
   },
   ["plenary.nvim"] = {
     loaded = true,
-    path = "/root/.local/share/nvim/site/pack/packer/start/plenary.nvim",
+    path = "/home/insait/.local/share/nvim/site/pack/packer/start/plenary.nvim",
     url = "https://github.com/nvim-lua/plenary.nvim"
   },
   ["telescope.nvim"] = {
     loaded = true,
-    path = "/root/.local/share/nvim/site/pack/packer/start/telescope.nvim",
+    path = "/home/insait/.local/share/nvim/site/pack/packer/start/telescope.nvim",
     url = "https://github.com/nvim-telescope/telescope.nvim"
   }
 }
